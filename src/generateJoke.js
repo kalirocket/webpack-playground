48fcab02e2fde26b5b1e9bfce272194cb27128cb
Siaw Henry Nartey <henrysiaw159@gmail.com>
2023-12-20 16:56:07 +0000

    CHORE: Boilerplate playground

---
+++ src/generateJoke.js
@@ -0,0 +1,3 @@
+const generateJoke = () => "I don't trust stairs. They're always up to something";
+
+export default generateJoke;
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: {
+        bundle: path.resolve(__dirname, 'src/index.js')
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        // Set contenthash for new port for old code
+        filename: '[name].[contenthash].js',
+        // Delete all unused hashed files
+        clean: true
+    },
+    // loaders for styles and css
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+    // Create html boilerplate
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Webpack App',
+            filename: 'index.html',
+            template: path.resolve(__dirname, 'src/template.html')  
+        })
+    ]
+}
