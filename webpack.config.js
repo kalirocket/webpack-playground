86c7a3000a4848d00489af908f3750cc715ea583
Siaw Henry Nartey <henrysiaw159@gmail.com>
2024-01-10 16:01:39 +0000

    FEAT: webpack build analyzer

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
     mode: 'production',
@@ -59,6 +60,7 @@ module.exports = {
             title: 'Webpack App',
             filename: 'index.html',
             template: path.resolve(__dirname, 'src/template.html')  
-        })
+        }),
+        new BundleAnalyzerPlugin()
     ]
 }
